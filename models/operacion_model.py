93bb1a81dadc2bdbf2450eaf8b7f57e53721e07e
Alberto <betofdez@gmail.com>
2026-06-28 14:25:05 +0200

    Incluida venta de acciones

--- models/operacion_model.py
+++ models/operacion_model.py
@@ -27,3 +27,30 @@ def insertar_compra(usuario_id, valor_id, numero_acciones, precio, gastos, fecha
 
     finally:
         conexion.close()
+
+def insertar_venta(usuario_id, valor_id, numero_acciones, precio, gastos, fecha, comentarios):
+    conexion = obtener_conexion()
+
+    try:
+        with conexion.cursor() as cursor:
+            sql = """
+                INSERT INTO operaciones
+                    (usuario_id, valor_id, tipo, numero_acciones, precio, gastos, fecha, comentarios)
+                VALUES
+                    (%s, %s, 'VENTA', %s, %s, %s, %s, %s)
+            """
+
+            cursor.execute(sql, (
+                usuario_id,
+                valor_id,
+                numero_acciones,
+                precio,
+                gastos,
+                fecha,
+                comentarios
+            ))
+
+            conexion.commit()
+
+    finally:
+        conexion.close()
